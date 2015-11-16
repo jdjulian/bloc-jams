85d418b449dcc773ba93aa4dabde945e3cd56311
Jean Juliano <jeanjuliano927@gmail.com>
2015-11-16 09:48:16 -0800

    added index to function

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,7 +2,7 @@ var animatePoints = function() {
                 
     var points = document.getElementsByClassName('point');
                 
-    var revealPoint = function {
+    var revealPoint = function(index) {
         points[index].style.opacity = 1;
         points[index].style.transform = "scaleX(1) translateY(0)";
         points[index].style.msTransform = "scaleX(1) translateY(0)";
@@ -10,7 +10,7 @@ var animatePoints = function() {
     }
     
     for (var i = 0; i < points.length; i++) {
-        revealPoint();
+        revealPoint(i);
     }
 };
                 
