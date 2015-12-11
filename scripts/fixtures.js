8e8ef2ec8e7ceff2b3dcc58deb0460f888724362
Jean Juliano <jeanjuliano927@gmail.com>
2015-12-11 11:19:26 -0800

    added fixtures. js

---
+++ scripts/fixtures.js
@@ -0,0 +1,44 @@
+ var albumPicasso = {
+     name: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { name: 'Blue', length: '4:26' },
+         { name: 'Green', length: '3:14' },
+         { name: 'Red', length: '5:01' },
+         { name: 'Pink', length: '3:21' },
+         { name: 'Magenta', length: '2:15' }
+     ]
+ };
+ 
+ var albumMarconi = {
+     name: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { name: 'Hello, Operator?', length: '1:01' },
+         { name: 'Ring, ring, ring', length: '5:01' },
+         { name: 'Fits in your pocket', length: '3:21' },
+         { name: 'Can you hear me now?', length: '3:14' },
+         { name: 'Wrong phone number', length: '2:15' }
+     ]
+ };
+
+var myAlbum = {
+    name: 'Learning To Code',
+    artist: 'Jean Juliano',
+    label: 'JJ',
+    year: '2015',
+    albumArtUrl: 'assets/images/album_covers/03.png',
+    songs: [
+        { name: 'HTML is like the Skeleton', length: '1:01' },
+        { name: 'CSS gives me color', length: '5:01' },
+        { name: 'Javascript makes me move', length: '3:21' },
+        { name: 'DOM Wha?', length: '3:14' },
+        { name: 'Too many JC libraries', length: '2:15' }
+    ]
+};
